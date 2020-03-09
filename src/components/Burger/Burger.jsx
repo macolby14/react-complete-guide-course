80965b2371b773d0998e2b79b14d435749b46456
Mark Colby <macolby14@gmail.com>
2020-03-08 14:11:01 -1000

    add burger ingredients to a list

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,14 +4,17 @@ import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
--- src/components/containers/BurgerBuilder/BuilderBurger.jsx
+++ src/components/containers/BurgerBuilder/BuilderBurger.jsx
@@ -3,12 +3,19 @@ import Aux from "../../../hoc/Aux";
 import Burger from "../../Burger/Burger";
 
 class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
+      meat: 3
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <div>
-          <Burger />
-        </div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
