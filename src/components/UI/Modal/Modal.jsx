5186efb73f0255888d35ec700251522bcdb048cc
Mark Colby <macolby14@gmail.com>
2020-03-09 12:39:01 -1000

    fixed Backdrop component by changing import

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import styles from "./Modal.module.css";
 import Aux from "../../../hoc/Aux";
-import Backdrop from "../Modal/Modal";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = props => {
   return (
     <Aux>
-      {/*<Backdrop show={props.show} />*/}
+      <Backdrop show={props.show} />
       <div
         className={styles.Modal}
         style={{
