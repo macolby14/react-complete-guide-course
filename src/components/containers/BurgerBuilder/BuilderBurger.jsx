4f2c3f6b6eefeb40f6aba7fba9106f8235f2fe7f
Mark Colby <macolby14@gmail.com>
2020-03-08 17:57:53 -1000

    added basic Builder controls

--- src/components/containers/BurgerBuilder/BuilderBurger.jsx
+++ src/components/containers/BurgerBuilder/BuilderBurger.jsx
@@ -1,22 +1,45 @@
 import React from "react";
+
 import Aux from "../../../hoc/Aux";
 import Burger from "../../Burger/Burger";
+import BuildControls from "../../Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
 
 class BurgerBuilder extends React.Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 3
-    }
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = type => {};
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
