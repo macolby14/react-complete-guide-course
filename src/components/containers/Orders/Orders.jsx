e2db07933f31c02cd27068fca0847e8206b7765d
Mark Colby <macolby14@gmail.com>
2020-04-26 19:48:11 -1000

    hook up Orders to Firebase

--- src/components/containers/Orders/Orders.jsx
+++ src/components/containers/Orders/Orders.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import axios from "../../../axios-orders";
+import WithErrorHandler from "../../../hoc/WithErrorHandler/WithErrorHandler";
 
 import Order from "../../../components/Order/Order";
 
 class Orders extends React.Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        this.setState({ orders: fetchedOrders, loading: false });
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +35,4 @@ class Orders extends React.Component {
   }
 }
 
-export default Orders;
+export default WithErrorHandler(Orders, axios);
